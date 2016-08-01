6492978282e63387e08b1d519b8cca1619856fdf
Cd <licd2009@outlook.com>
2016-08-01 12:05:08 -0400

    third commit to see diff

--- src/com/testCommit/testCommit.java
+++ src/com/testCommit/testCommit.java
@@ -3,8 +3,9 @@ package com.testCommit;
 public class testCommit {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("test commit");
+		System.out.println("commit");
+		// Check diff
+		System.out.println("check difference");
 	}
 
 }
